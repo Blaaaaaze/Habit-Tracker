8262e5536c38ac7dce4eea2ed8780d08c6926ea7
Igor <igorsmirnov604@gmail.com>
2026-07-29 22:05:10 +0300

    feat: Сделана прокрутка по движению мыши и в реадми поставлены оставшиеся цели для проекта

--- src/features/tasks/HabbitList/HabbitList.tsx
+++ src/features/tasks/HabbitList/HabbitList.tsx
@@ -3,12 +3,44 @@ import Card from "../Card/Card";
 import styles from './HabbitList.module.scss';
 import { getActiveHabits, getCanceledHabits, getCompletedHabits } from "../task-selectors";
 import {AnimatePresence} from 'motion/react';
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 const HabbitList = () => {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const dragStatus = useRef({
+        isDragging: false,
+        startX: 0,
+        scrollLeft: 0
+    });
+
     const activeHabits = useSelector(getActiveHabits);
     const completedHabits = useSelector(getCompletedHabits);
     const canceledHabits = useSelector(getCanceledHabits);
+
+    const handlePointerDown = (e: React.PointerEvent<HTMLDivElement>) => {
+        const container = containerRef.current;
+        if (!container) return;
+
+        dragStatus.current.isDragging = true;
+        dragStatus.current.startX = e.clientX;
+        dragStatus.current.scrollLeft = container.scrollLeft;
+
+        container.setPointerCapture(e.pointerId);
+    }
+
+    const handlePointerMove = (e: React.PointerEvent<HTMLDivElement>) => {
+        const container = containerRef.current;
+
+        if (!container || dragStatus.current.isDragging) return;
+
+        const delta = e.clientX - dragStatus.current.startX;
+
+        container.scrollLeft = dragStatus.current.scrollLeft - delta;
+    }
+
+    const stopDragging = () => {
+        dragStatus.current.isDragging = false;
+    }
     
     useEffect(() => {
         localStorage.setItem('habits', JSON.stringify([...activeHabits, ...completedHabits, ...canceledHabits]));
@@ -17,7 +49,15 @@ const HabbitList = () => {
     return (
         <>
             <h2 className={styles['category-subtitle']}>Активные привычки</h2>
-            <div className={styles.card__container}>
+            <div
+            ref={containerRef}
+            className={styles.card__container}
+            onPointerDown={(e) => handlePointerDown(e)}
+            onPointerMove={(e) => handlePointerMove(e)}
+            onPointerUp={stopDragging}
+            onPointerLeave={stopDragging}
+            onPointerCancel={stopDragging}
+            >
                 <AnimatePresence >
                     {
                         activeHabits.length ? 
